46ba48f0615ade6bc77ca17746faabeac381c3fe
Matt Thompson <matthew.thompson.a@gmail.com>
2023-08-07 21:19:24 -0400

    migration for content url

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_07_013054) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_08_011323) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,6 +33,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_07_013054) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "url", null: false
+    t.index ["url"], name: "index_posts_on_url", unique: true
   end
 
 end
