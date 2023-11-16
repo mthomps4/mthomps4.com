e512559901b6141b67dc588c9af1889bf2fa211a
Matt Thompson <matthew.thompson.a@gmail.com>
2023-11-16 00:42:57 +0000

    lint fixed

--- app/helpers/theme/checkbox_helper.rb
+++ app/helpers/theme/checkbox_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Theme
-  module MarkdownContainerHelper
+  module CheckboxHelper
     module_function
 
     def validate_options(options)
@@ -22,7 +22,7 @@ module Theme
     end
 
     def default_styles
-      'w-full p-4 rounded-lg break-words prose prose-sm sm:prose-base prose-brand-blue dark:prose-invert prose-img:rounded-lg hover:prose-a:text-brand-key-lime-200 bg-gradient-to-br from-brand-blue-50 to-brand-blue-100 dark:from-brand-blue-800 dark:to-brand-blue-950 border-[1px] border-brand-blue-300 dark:border-brand-blue-600 text-brand-blue-950 dark:text-brand-blue-100'
+      'h-4 w-4 rounded border-gray-300 text-brand-blue-600 focus:ring-brand-blue-600'
     end
   end
 end
