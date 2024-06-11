0cfe2d093337938517135f0bd79191f1eeb679e9
Matt Thompson <matthew.thompson.a@gmail.com>
2024-06-10 22:48:54 -0400

    feat image > og WIP

--- tailwind.config.js
+++ tailwind.config.js
@@ -72,6 +72,9 @@ export default {
   plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms')],
   theme: {
     extend: {
+      screens: {
+        '3xl': '1600px',
+      },
       fontFamily: {
         brand: ['Satoshi', fontFamily.sans],
       },
