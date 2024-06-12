55048c39453dd11b8dc6467f4d75583feababdb8
Matt Thompson <matthew.thompson.a@gmail.com>
2024-06-11 23:54:27 -0400

    cleanup

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,9 +2,11 @@
 
 class AdminController < ApplicationController
   before_action :authenticate_user!
-
-  # http_basic_authenticate_with name: Rails.application.credentials.admin[:user],
-  #                              password: Rails.application.credentials.admin[:password]
+  before_action :set_admin_meta_tags
 
   def index; end
+
+  def set_admin_meta_tags
+    set_meta_tags noindex: true, nofollow: true
+  end
 end
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -14,39 +14,12 @@ class MainController < ApplicationController
     @resume = Resume.last
   end
 
-  def new_contact
-    @contact = Contact.new
-  end
-
-  def create_contact
-    @contact = Contact.new(contact_params)
-
-    if @contact.save
-      redirect_to root_path, notice: 'Thank You!'
-    else
-      redirect_to root_path
-    end
-  end
-
-  def blog; end
-
   def digital_forge
     @q = Post.published.ransack(params[:q])
     @posts = @q.result(distinct: true).order(published_on: :desc).page(params[:page]).per(10)
     @title = 'The Digital Forge'
     @search_path = digital_forge_path
-
-    render :blog
   end
-
-  # def hand_tool_armory
-  #   @q = Post.hand_tool_armory.published.ransack(params[:q])
-  #   @posts = @q.result(distinct: true).order(published_on: :desc).page(params[:page]).per(10)
-  #   @title = 'The Hand Tool Armory'
-  #   @search_path = hand_tool_armory_path
-
-  #   render :blog
-  # end
 
   def hand_tool_armory; end
 
